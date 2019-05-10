193815c3f282218c218e04ab9d574db5b1d946cc
Benoît Chantre <bc@s-agence.ch>
2019-05-10 13:28:12 +0200

    Add footer branding

---
+++ template-parts/footer/branding.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying the footer branding
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<div class="footer-branding row alignwide">
+	<img class="footer-logo" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/voxia-communication.svg' ); ?>" alt="Voxia communication" />
+	<div class="social-links">social menu</div>
+</div><!-- .footer-branding -->
